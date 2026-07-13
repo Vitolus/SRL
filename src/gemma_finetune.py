1058edd9235ce03455d81241344f6b1c5fadbe83
Vitolus <vito.davide00@gmail.com>
2026-07-13 16:14:48 +0200

    enable packing with `bfd` strategy, remove unused `max_length` argument

--- src/gemma_finetune.py
+++ src/gemma_finetune.py
@@ -119,7 +119,8 @@ def train(train_langs, srl_type, model_name, run_name, models_dir):
         gradient_accumulation_steps=8,
         gradient_checkpointing=True,
         dataset_text_field="text",
-        packing=False,
+        packing=True,
+        packing_strategy="bfd",
         warmup_ratio=0.03,
         lr_scheduler_type="cosine",
         neftune_noise_alpha=5
@@ -129,7 +130,6 @@ def train(train_langs, srl_type, model_name, run_name, models_dir):
     trainer = SFTTrainer(
         model=model,
         args=training_args,
-        max_length=None,
         train_dataset=train_ds,
         eval_dataset=val_ds,
         processing_class=tokenizer
